c17db58c15e4b334c6e35b9fe2ab453624a175ef
Dmytro Chasovskyi <dmytro.chasovskyi@greenbird.com>
2020-05-17 16:44:20 +0200

    Closes #9. Add list storage bucket and bucket names functionality

--- storage_bucket/list_buckets.py
+++ storage_bucket/list_buckets.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Code to safely list storage buckets."""
-from typing import Optional, Set
+from typing import Iterator, Optional, Set, Union
 
 from attr import dataclass
 from google.cloud.storage import Bucket, Client
@@ -19,18 +19,21 @@ class ListBuckets(object):
 
       >>> from returns.pipeline import is_successful
       >>> assert ListBuckets()()
-      >>> assert is_successful(ListBuckets()('test'))
+      >>> assert is_successful(ListBuckets()(prefix='test'))
     """
 
     _client = Client
 
     @safe
-    def __call__(
+    def __call__(  # noqa: WPS211
         self,
+        max_results: Optional[int] = None,
+        page_token: Optional[str] = None,
         prefix: Optional[str] = None,
         fields: Optional[Set] = None,
         projection: str = 'noAcl',
         project: Optional[str] = None,
+        timeout: Union[float, int] = 60,
     ) -> Set[Bucket]:
         """List the storage bucket files."""
         client = self._initialize_client().alt(  # type: ignore
@@ -38,10 +41,13 @@ class ListBuckets(object):
         ).unwrap()
 
         return client.list_buckets(
+            max_results=max_results,
+            page_token=page_token,
             prefix=prefix,
             fields=fields,
             projection=projection,
             project=project,
+            timeout=timeout,
         )
 
     @safe
@@ -49,21 +55,35 @@ class ListBuckets(object):
         return self._client()
 
 
-def list_buckets(
+def list_buckets(  # noqa: WPS211
+    max_results: Optional[int] = None,
+    page_token: Optional[str] = None,
     prefix: Optional[str] = None,
     fields: Optional[Set] = None,
     projection: str = 'noAcl',
     project: Optional[str] = None,
+    timeout: Union[float, int] = 60,
 ) -> Bucket:
     """List Buckets, but return Set[Bucket] instead of ResultE[Set[Bucket]].
 
     Raise exception when Modal is in failure state.
     """
     return ListBuckets()(
+        max_results=max_results,
+        page_token=page_token,
         prefix=prefix,
         fields=fields,
         projection=projection,
         project=project,
+        timeout=timeout,
     ).alt(
         raise_exception,
     ).unwrap()
+
+
+def list_bucket_names(buckets: Set[Bucket]) -> Iterator[str]:
+    """Iterate over Buckets and retrieve their names.
+
+    Raise NoneType Exception when Bucket is None.
+    """
+    return map(lambda bucket: bucket.name, buckets)

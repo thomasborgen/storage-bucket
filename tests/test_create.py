c7f0d40a4f426ba32d315bbeda61968d00119334
Thomas Borgen <thomas.borgen@greenbird.com>
2020-06-12 21:00:31 +0200

    we get a too many buckets created/deleted error when testing. So try deleting test for the modal version since we are using that code in the function anyways

--- tests/test_create.py
+++ tests/test_create.py
@@ -3,18 +3,9 @@ from google.api_core.exceptions import Conflict
 from google.cloud.storage import Bucket
 from typing_extensions import Final
 
-from storage_bucket.create import CreateBucket, create_bucket
+from storage_bucket.create import create_bucket
 
 LOCATION: Final[str] = 'EU'
-
-
-def test_create_bucket_modal(creatable_bucket):
-    """Create bucket returns Success(Bucket)."""
-    bucket_result = CreateBucket()(
-        storage_bucket_name=creatable_bucket,
-        location=LOCATION,
-    )
-    assert isinstance(bucket_result.unwrap(), Bucket)
 
 
 def test_create_bucket_function(creatable_bucket):
@@ -24,15 +15,6 @@ def test_create_bucket_function(creatable_bucket):
         location=LOCATION,
     )
     assert isinstance(bucket_result, Bucket)
-
-
-def test_create_bucket_modal_failure(existing_bucket):
-    """Conflicting name returns Failure(Conflict)."""
-    bucket_result = CreateBucket()(
-        storage_bucket_name=existing_bucket,
-        location=LOCATION,
-    )
-    assert isinstance(bucket_result.failure(), Conflict)
 
 
 def test_create_bucket_function_exception(existing_bucket):
